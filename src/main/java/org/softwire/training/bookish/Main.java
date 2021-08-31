f0a3e5cc806a30b0492045a8b0dc4690a27c863e
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-08-31 16:24:31 +0100

    use mysql connector v8, JDBC connect and query

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -2,33 +2,55 @@ package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Properties;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
-        String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
+        String hostname = "localhost:3306";
+        String database = "djna";
+        String user = "booker";
+        String password = "booker";
+        String connectionString = "jdbc:mysql://" + hostname
+                + "/djna";
+                //"&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
-        jdbcMethod(connectionString);
-        jdbiMethod(connectionString);
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+        connectionProps.setProperty("useSSL", "false");
+
+        jdbcMethod(connectionString, connectionProps);
+        //jdbiMethod(connectionString);
     }
 
-    private static void jdbcMethod(String connectionString) throws SQLException {
+    private static void jdbcMethod(String connectionString, Properties connectionProps) throws SQLException {
         System.out.println("JDBC method...");
 
         // TODO: print out the details of all the books (using JDBC)
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
-        Connection connection = DriverManager.getConnection(connectionString);
+        Connection connection = DriverManager.getConnection(connectionString, connectionProps);
+        System.out.println("JDBC connected ...");
+        String query = "select id, title, author from books";
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(query);
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String title = resultSet.getString("title");
+                    String author = resultSet.getString("author");
+
+                    System.out.printf("%d: %s by %s\n", id, title, author);
+                }
+            } catch (SQLException e) {
+                System.out.printf("Query Exception " + e);
+            }
 
 
+        connection.close();
+        System.out.println("JDBC connection closed.");
 
     }
 
