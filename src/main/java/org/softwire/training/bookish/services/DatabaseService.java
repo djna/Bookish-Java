18094d0da7309200e4a765d8fd71673b9c6b3dcc
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-09-10 10:09:29 +0100

    refactor properties

--- src/main/java/org/softwire/training/bookish/services/DatabaseService.java
+++ src/main/java/org/softwire/training/bookish/services/DatabaseService.java
@@ -2,13 +2,26 @@ package org.softwire.training.bookish.services;
 
 import org.jdbi.v3.core.Jdbi;
 
+import java.util.Properties;
+
 public abstract class DatabaseService {
 
     private final String hostname = "localhost";
     private final String database = System.getenv("DB_NAME");
     private final String user = System.getenv("DB_USER");
     private final String password = System.getenv("DB_PASS");
-    private final String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
+    private final String connectionString = "jdbc:mysql://" + hostname + "/" + database;
 
-    protected final Jdbi jdbi = Jdbi.create(connectionString);
+    private Properties makeConnectiohPropertions() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+        connectionProps.setProperty("useSSL", "false");
+        connectionProps.setProperty("useJDBCCompliantTimezoneShift", "true");
+        connectionProps.setProperty("useLegacyDatetimeCode", "false");
+        connectionProps.setProperty("serverTimezone=GMT&useSSL", "false");
+        return connectionProps;
+    }
+
+    protected final Jdbi jdbi = Jdbi.create(connectionString, makeConnectiohPropertions());
 }
